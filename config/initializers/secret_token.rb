581e2c34182657af8c3e0b81f8a16f8a207bdb57
Vladislav Moskovets <github@vlad.org.ua>
2010-10-19 16:25:03 +0300

    initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loaner::Application.config.secret_token = '9c51a8c5424bc37db1f7e05a5161b6f4a199fbf40c298bcc4a6fd90df93eafcf5b97f7f4baad11aa3856bfab057ddd1b4c11dec70744a4ccbe538ee34f466c3b'

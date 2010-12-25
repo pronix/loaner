a6547dd8178285c2f2e53408c2a986304a0a210d
Vladislav Moskovets <github@vlad.org.ua>
2010-12-25 07:12:53 +0200

    reports update

--- app/controllers/reports/for_records/cash_account_books_controller.rb
+++ app/controllers/reports/for_records/cash_account_books_controller.rb
@@ -1,5 +1,9 @@
 class Reports::ForRecords::CashAccountBooksController < Reports::BaseController
   def create
+    show
+  end
+
+  def show
     @reports_params = Reports::Params.new params[:reports_params]
     @transactions = @reports_params.book.transactions.where(["date >= ? AND date < ?", @reports_params.date_start, @reports_params.date_end])
   end
--- app/controllers/reports/for_records/loan_account_books_controller.rb
+++ app/controllers/reports/for_records/loan_account_books_controller.rb
@@ -1,2 +1,7 @@
 class Reports::ForRecords::LoanAccountBooksController < ApplicationController
+  def show
+    @reports_params = Reports::Params.new params[:reports_params]
+    @loan = @reports_params.loan
+    @transactions = @loan.transactions
+  end
 end

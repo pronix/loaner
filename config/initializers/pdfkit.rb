039c526cb699c77935efa9f05c365c4dd7c505b8
Vladislav Moskovets <github@vlad.org.ua>
2011-02-10 17:46:12 +0200

    fix pdf paper fields

---
+++ config/initializers/pdfkit.rb
@@ -0,0 +1,11 @@
+# config/initializers/pdfkit.rb
+PDFKit.configure do |config|
+  config.default_options = {
+    :page_size => 'A4',
+    :print_media_type => true,
+    :margin_top => '0.02in',                                                                                                                                                                           
+    :margin_right => '0.02in',                                                                                                                                                                         
+    :margin_bottom => '0.02in',                                                                                                                                                                        
+    :margin_left => '0.02in',         
+  }
+end

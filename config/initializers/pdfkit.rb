569efda0fa2c7665c824409ccef43e9a8d5a555d
Vladislav Moskovets <github@vlad.org.ua>
2011-02-13 01:52:10 +0200

    landscape page

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -2,6 +2,7 @@
 PDFKit.configure do |config|
   config.default_options = {
     :page_size => 'A4',
+    :orientation => "Landscape",
     :print_media_type => true,
     :margin_top => '0.02in',                                                                                                                                                                           
     :margin_right => '0.02in',                                                                                                                                                                         
